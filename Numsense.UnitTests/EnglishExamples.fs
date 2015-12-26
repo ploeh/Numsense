36f78ae8ff5d8d794d035b874382f9d05d7ba26a
Mark Seemann <mark@ploeh.dk>
2015-12-26 18:55:16 +0100

    Added one-nine English examples.

--- Numsense.UnitTests/EnglishExamples.fs
+++ Numsense.UnitTests/EnglishExamples.fs
@@ -4,7 +4,16 @@ open Xunit
 open Swensen.Unquote
 
 [<Theory>]
-[<InlineData("zero", 0)>]
+[<InlineData( "zero", 0)>]
+[<InlineData(  "one", 1)>]
+[<InlineData(  "two", 2)>]
+[<InlineData("three", 3)>]
+[<InlineData( "four", 4)>]
+[<InlineData( "five", 5)>]
+[<InlineData(  "six", 6)>]
+[<InlineData("seven", 7)>]
+[<InlineData("eight", 8)>]
+[<InlineData( "nine", 9)>]
 let ``ofEnglish returns correct result`` (english : string, expected : int) =
     let actual = Numeral.ofEnglish english
     expected =! actual
--- Numsense/Numeral.fs
+++ Numsense/Numeral.fs
@@ -4,6 +4,15 @@ let toEnglish x = string x
 
 let ofEnglish x = 
     match x with
-    | "zero" -> 0
+    | "zero"  -> 0
+    | "one"   -> 1
+    | "two"   -> 2
+    | "three" -> 3
+    | "four"  -> 4
+    | "five"  -> 5
+    | "six"   -> 6
+    | "seven" -> 7
+    | "eight" -> 8
+    | "nine"  -> 9
     | _ -> System.Int32.Parse x
 
