e444139192e54c27e0d452c4c60c739d4d34feb7
Mark Seemann <mark@ploeh.dk>
2015-12-26 17:26:47 +0100

    Added a build script.

---
+++ Build.fsx
@@ -0,0 +1,25 @@
+﻿#r @"packages/FAKE.4.11.3/tools/FakeLib.dll"
+
+open Fake
+open Fake.Testing
+
+Target "Clean" (fun _ ->
+    directExec (fun info ->
+        info.FileName <- "git"
+        info.Arguments <- "clean -xdf")
+    |> ignore)
+
+Target "Build" (fun _ ->
+    !! "Numsense.sln"
+    |> MSBuildDebug "" "Rebuild"
+    |> ignore)
+
+Target "Test" (fun _ ->
+    !! "*/bin/Debug/*Ploeh.*.*Tests.dll"
+    |> xUnit2 (fun p -> { p with Parallel = ParallelMode.All }))
+
+"Clean"
+==> "Build"
+==> "Test"
+
+RunTargetOrDefault "Test"
