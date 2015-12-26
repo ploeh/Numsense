aa8b9d52dbe6f68a7f63fc7542e77eec49742c31
Mark Seemann <mark@ploeh.dk>
2015-12-26 18:02:46 +0100

    Added the property that one func is inverse
    of other.

---
+++ Numsense.UnitTests/NumeralProperties.fs
@@ -0,0 +1,9 @@
+﻿module Ploeh.Numsense.NumeralProperties
+
+open FsCheck.Xunit
+open Swensen.Unquote
+
+[<Property(QuietOnSuccess = true)>]
+let ``ofEnglish is the inverse of toEnglish`` (x : int) =
+    test <@ x = (x |> Numeral.toEnglish |> Numeral.ofEnglish) @>
+
---
+++ Numsense/Numeral.fs
@@ -0,0 +1,6 @@
+﻿module Ploeh.Numsense.Numeral
+
+let toEnglish x = string x
+
+let ofEnglish x = System.Int32.Parse x
+
