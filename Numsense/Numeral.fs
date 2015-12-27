87b08e29e6d3ff9a2b5d7b8bf01440880e18749f
Mark Seemann <mark@ploeh.dk>
2015-12-27 17:39:54 +0100

    Refactored to use an active pattern,
    which makes the implementation far more readable, and, from an extremely
    superficial measurement, doesn't seem to negatively impact performance.

--- Numsense/Numeral.fs
+++ Numsense/Numeral.fs
@@ -6,42 +6,46 @@ let ofEnglish x =
     let (%*) factor x =
         let multiplicand = x % factor
         x + (factor * multiplicand) - multiplicand
+
+    let (|StartsWith|_|) prefix (candidate : string) =
+        if candidate.StartsWith prefix
+        then Some (candidate.Substring prefix.Length)
+        else None
+
     let rec conv acc xs =        
         match xs with
-        | []                                         -> acc
-        | 'Z'::'E'::'R'::'O'::t                      -> conv  (0  + acc) t
-        | 'O'::'N'::'E'::t                           -> conv  (1  + acc) t
-        | 'T'::'W'::'O'::t                           -> conv  (2  + acc) t
-        | 'T'::'H'::'R'::'E'::'E'::t                 -> conv  (3  + acc) t
-        | 'F'::'O'::'U'::'R'::t                      -> conv  (4  + acc) t
-        | 'F'::'I'::'V'::'E'::t                      -> conv  (5  + acc) t
-        | 'S'::'I'::'X'::t                           -> conv  (6  + acc) t
-        | 'S'::'E'::'V'::'E'::'N'::t                 -> conv  (7  + acc) t
-        | 'E'::'I'::'G'::'H'::'T'::t                 -> conv  (8  + acc) t
-        | 'N'::'I'::'N'::'E'::t                      -> conv  (9  + acc) t
-        | 'T'::'E'::'N'::t                           -> conv (10  + acc) t
-        | 'E'::'L'::'E'::'V'::'E'::'N'::t            -> conv (11  + acc) t
-        | 'T'::'W'::'E'::'L'::'V'::'E'::t            -> conv (12  + acc) t
-        | 'T'::'H'::'I'::'R'::'T'::'E'::'E'::'N'::t  -> conv (13  + acc) t
-        | 'F'::'I'::'F'::'T'::'E'::'E'::'N'::t       -> conv (15  + acc) t
-        | 'E'::'E'::'N'::t // matches 'een' in 'eighteen'         
-        | 'T'::'E'::'E'::'N'::t                      -> conv (10  + acc) t
-        | 'T'::'W'::'E'::'N'::'T'::'Y'::t            -> conv (20  + acc) t
-        | 'T'::'H'::'I'::'R'::'T'::'Y'::t            -> conv (30  + acc) t
-        | 'F'::'O'::'R'::'T'::'Y'::t                 -> conv (40  + acc) t
-        | 'F'::'I'::'F'::'T'::'Y'::t                 -> conv (50  + acc) t
-        | 'Y'::t // matches 'y' in 'eighty'
-        | 'T'::'Y'::t                                -> conv (10 %* acc) t
-        | 'H'::'U'::'N'::'D'::'R'::'E'::'D'::t       ->
-            conv (if acc = 0 then  100 else  100 %* acc) t
-        | 'T'::'H'::'O'::'U'::'S'::'A'::'N'::'D'::t  ->
-            conv (if acc = 0 then 1000 else 1000 %* acc) t
-        | 'M'::'I'::'L'::'L'::'I'::'O'::'N'::t       ->
-            conv (   1000000 %* acc) t
-        | 'B'::'I'::'L'::'L'::'I'::'O'::'N'::t       ->
-            conv (1000000000  * acc) t
+        | ""                      -> acc
+        | StartsWith "ZERO"     t -> conv          (0  + acc) t
+        | StartsWith "ONE"      t -> conv          (1  + acc) t
+        | StartsWith "TWO"      t -> conv          (2  + acc) t
+        | StartsWith "THREE"    t -> conv          (3  + acc) t
+        | StartsWith "FOUR"     t -> conv          (4  + acc) t
+        | StartsWith "FIVE"     t -> conv          (5  + acc) t
+        | StartsWith "SIX"      t -> conv          (6  + acc) t
+        | StartsWith "SEVEN"    t -> conv          (7  + acc) t
+        | StartsWith "EIGHT"    t -> conv          (8  + acc) t
+        | StartsWith "NINE"     t -> conv          (9  + acc) t
+        | StartsWith "TEN"      t -> conv         (10  + acc) t
+        | StartsWith "ELEVEN"   t -> conv         (11  + acc) t
+        | StartsWith "TWELVE"   t -> conv         (12  + acc) t
+        | StartsWith "THIRTEEN" t -> conv         (13  + acc) t
+        | StartsWith "FIFTEEN"  t -> conv         (15  + acc) t
+        | StartsWith "EEN"      t // matches 'een' in 'eighteen'         
+        | StartsWith "TEEN"     t -> conv         (10  + acc) t
+        | StartsWith "TWENTY"   t -> conv         (20  + acc) t
+        | StartsWith "THIRTY"   t -> conv         (30  + acc) t
+        | StartsWith "FORTY"    t -> conv         (40  + acc) t
+        | StartsWith "FIFTY"    t -> conv         (50  + acc) t
+        | StartsWith "Y"        t // matches 'y' in 'eighty'
+        | StartsWith "TY"       t -> conv         (10 %* acc) t
+        | StartsWith "HUNDRED"  t ->
+            conv (if acc = 0 then  100 else       100 %* acc) t
+        | StartsWith "THOUSAND" t ->
+            conv (if acc = 0 then 1000 else      1000 %* acc) t
+        | StartsWith "MILLION"  t -> conv    (1000000 %* acc) t
+        | StartsWith "BILLION"  t -> conv (1000000000  * acc) t
         | _ -> -1
 
     match System.Int32.TryParse x with
     | true, i -> i
-    | _ -> conv 0 (x.Trim().ToUpper(System.Globalization.CultureInfo "en") |> Seq.toList)
+    | _ -> conv 0 (x.Trim().ToUpper(System.Globalization.CultureInfo "en"))
