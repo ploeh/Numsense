8d0021c81674b983ec224656a56c070ef07eec33
Mark Seemann <mark@ploeh.dk>
2015-12-27 17:06:57 +0100

    Refactored by introducing a custom operator.
    The symbol `%*` was chosen because the operation involves both modulo and
    multiplication, and can be read 'modulo-multiply'.

--- Numsense/Numeral.fs
+++ Numsense/Numeral.fs
@@ -2,53 +2,44 @@
 
 let toEnglish x = string x
 
-let ofEnglish x = 
-    // Multiply the 'ty's
-    let multys x =
-        let ones = x % 10
-        x + (10 * ones) - ones
-    let multiplyHundreds x =
-        let tens = x % 100
-        x + (100 * tens) - tens
-    let multiplyThousands x =
-        let hundreds = x % 1000
-        x + (1000 * hundreds) - hundreds
-    let multiplyMillions x =
-        let thousands = x % 1000000
-        x + (1000000 * thousands) - thousands
+let ofEnglish x =
+    let (%*) factor x =
+        let multiplicand = x % factor
+        x + (factor * multiplicand) - multiplicand
     let rec conv acc xs =        
         match xs with
         | []                                         -> acc
-        | 'Z'::'E'::'R'::'O'::t                      -> conv  (0 + acc) t
-        | 'O'::'N'::'E'::t                           -> conv  (1 + acc) t
-        | 'T'::'W'::'O'::t                           -> conv  (2 + acc) t
-        | 'T'::'H'::'R'::'E'::'E'::t                 -> conv  (3 + acc) t
-        | 'F'::'O'::'U'::'R'::t                      -> conv  (4 + acc) t
-        | 'F'::'I'::'V'::'E'::t                      -> conv  (5 + acc) t
-        | 'S'::'I'::'X'::t                           -> conv  (6 + acc) t
-        | 'S'::'E'::'V'::'E'::'N'::t                 -> conv  (7 + acc) t
-        | 'E'::'I'::'G'::'H'::'T'::t                 -> conv  (8 + acc) t
-        | 'N'::'I'::'N'::'E'::t                      -> conv  (9 + acc) t
-        | 'T'::'E'::'N'::t                           -> conv (10 + acc) t
-        | 'E'::'L'::'E'::'V'::'E'::'N'::t            -> conv (11 + acc) t
-        | 'T'::'W'::'E'::'L'::'V'::'E'::t            -> conv (12 + acc) t
-        | 'T'::'H'::'I'::'R'::'T'::'E'::'E'::'N'::t  -> conv (13 + acc) t
-        | 'F'::'I'::'F'::'T'::'E'::'E'::'N'::t       -> conv (15 + acc) t
-        | 'E'::'E'::'N'::t // matches 'een' in 'eighteen'
-        | 'T'::'E'::'E'::'N'::t                      -> conv (10 + acc) t
-        | 'T'::'W'::'E'::'N'::'T'::'Y'::t            -> conv (20 + acc) t
-        | 'T'::'H'::'I'::'R'::'T'::'Y'::t            -> conv (30 + acc) t
-        | 'F'::'O'::'R'::'T'::'Y'::t                 -> conv (40 + acc) t
-        | 'F'::'I'::'F'::'T'::'Y'::t                 -> conv (50 + acc) t
+        | 'Z'::'E'::'R'::'O'::t                      -> conv  (0  + acc) t
+        | 'O'::'N'::'E'::t                           -> conv  (1  + acc) t
+        | 'T'::'W'::'O'::t                           -> conv  (2  + acc) t
+        | 'T'::'H'::'R'::'E'::'E'::t                 -> conv  (3  + acc) t
+        | 'F'::'O'::'U'::'R'::t                      -> conv  (4  + acc) t
+        | 'F'::'I'::'V'::'E'::t                      -> conv  (5  + acc) t
+        | 'S'::'I'::'X'::t                           -> conv  (6  + acc) t
+        | 'S'::'E'::'V'::'E'::'N'::t                 -> conv  (7  + acc) t
+        | 'E'::'I'::'G'::'H'::'T'::t                 -> conv  (8  + acc) t
+        | 'N'::'I'::'N'::'E'::t                      -> conv  (9  + acc) t
+        | 'T'::'E'::'N'::t                           -> conv (10  + acc) t
+        | 'E'::'L'::'E'::'V'::'E'::'N'::t            -> conv (11  + acc) t
+        | 'T'::'W'::'E'::'L'::'V'::'E'::t            -> conv (12  + acc) t
+        | 'T'::'H'::'I'::'R'::'T'::'E'::'E'::'N'::t  -> conv (13  + acc) t
+        | 'F'::'I'::'F'::'T'::'E'::'E'::'N'::t       -> conv (15  + acc) t
+        | 'E'::'E'::'N'::t // matches 'een' in 'eighteen'         
+        | 'T'::'E'::'E'::'N'::t                      -> conv (10  + acc) t
+        | 'T'::'W'::'E'::'N'::'T'::'Y'::t            -> conv (20  + acc) t
+        | 'T'::'H'::'I'::'R'::'T'::'Y'::t            -> conv (30  + acc) t
+        | 'F'::'O'::'R'::'T'::'Y'::t                 -> conv (40  + acc) t
+        | 'F'::'I'::'F'::'T'::'Y'::t                 -> conv (50  + acc) t
         | 'Y'::t // matches 'y' in 'eighty'
-        | 'T'::'Y'::t                                -> conv (multys acc) t
+        | 'T'::'Y'::t                                -> conv (10 %* acc) t
         | 'H'::'U'::'N'::'D'::'R'::'E'::'D'::t       ->
-            conv (if acc = 0 then 100 else multiplyHundreds acc) t
+            conv (if acc = 0 then 100 else 100 %* acc) t
         | 'T'::'H'::'O'::'U'::'S'::'A'::'N'::'D'::t  ->
-            conv (if acc = 0 then 1000 else multiplyThousands acc) t
+            conv (if acc = 0 then 1000 else 1000 %* acc) t
         | 'M'::'I'::'L'::'L'::'I'::'O'::'N'::t       ->
-            conv (multiplyMillions acc) t
-        | 'B'::'I'::'L'::'L'::'I'::'O'::'N'::t       -> conv (1000000000 * acc) t
+            conv (1000000 %* acc) t
+        | 'B'::'I'::'L'::'L'::'I'::'O'::'N'::t       ->
+            conv (1000000000 * acc) t
         | _ -> -1
 
     match System.Int32.TryParse x with
