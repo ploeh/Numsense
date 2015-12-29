b967a5dba9e4643ad57cc129abc9a04d726c1174
Mark Seemann <mark@ploeh.dk>
2015-12-29 23:50:23 +0100

    Aligned language names,
    in order to make it even clearer how the file is organised.

--- Numsense/Numeral.fs
+++ Numsense/Numeral.fs
@@ -2,8 +2,8 @@
 
 open Ploeh.Numsense.InternalDsl
 
-let toDanish = Danish.toDanishImp 1
-let tryParseDanish = Danish.tryParseDanishImp
+let toDanish =        Danish.toDanishImp 1
+let tryParseDanish =  Danish.tryParseDanishImp
 
-let toEnglish = English.toEnglishImp
+let toEnglish =       English.toEnglishImp
 let tryParseEnglish = English.tryParseEnglishImp
