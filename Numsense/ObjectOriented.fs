501021ef5c7c0b9dbe81fb1a08b1065a2fef4289
Mark Seemann <mark@ploeh.dk>
2015-12-30 12:40:47 +0100

    Added EnglishNumericalConverter class,
    for C# access.

---
+++ Numsense/ObjectOriented.fs
@@ -0,0 +1,17 @@
+﻿namespace Ploeh.Numsense.ObjectOriented
+
+open System.Runtime.InteropServices
+open Ploeh.Numsense
+
+type INumeralConverter =
+    abstract ToNumeral : number : int -> string
+    abstract TryParse : s : string * [<Out>]result : int byref -> bool
+
+type EnglishNumeralConverter () =
+    interface INumeralConverter with
+        member this.ToNumeral number = Numeral.toEnglish number
+        member this.TryParse (s, result) = 
+            match s = null, lazy (Numeral.tryParseEnglish s) with
+            | true, _
+            | false, Lazy None -> false
+            | false, Lazy (Some i) -> result <- i; true
