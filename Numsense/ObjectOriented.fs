d869328cfc368d6d55b493be9a0cd6579c59dc58
Max Malook <community@malook.de>
2016-01-04 12:01:19 +0100

    remove tryParse functionality duplication in OO code

--- Numsense/ObjectOriented.fs
+++ Numsense/ObjectOriented.fs
@@ -7,32 +7,30 @@ type INumeralConverter =
     abstract ToNumeral : number : int -> string
     abstract TryParse : s : string * [<Out>]result : int byref -> bool
 
+module internal Helper =
+    let tryParse f (s, result : int byref) =
+        match isNull s, lazy (f s) with
+        | true, _
+        | false, Lazy None -> false
+        | false, Lazy (Some i) -> result <- i; true
+
 type EnglishNumeralConverter () =
     interface INumeralConverter with
         member this.ToNumeral number = Numeral.toEnglish number
         member this.TryParse (s, result) = 
-            match isNull s, lazy (Numeral.tryParseEnglish s) with
-            | true, _
-            | false, Lazy None -> false
-            | false, Lazy (Some i) -> result <- i; true
+            Helper.tryParse Numeral.tryParseEnglish (s, &result)
 
 type DanishNumeralConverter () =
     interface INumeralConverter with
         member this.ToNumeral number = Numeral.toDanish number
         member this.TryParse (s, result) =
-            match isNull s, lazy (Numeral.tryParseDanish s) with
-            | true, _
-            | false, Lazy None -> false
-            | false, Lazy (Some i) -> result <- i; true
+            Helper.tryParse Numeral.tryParseDanish (s, &result)
 
 type FarsiNumeralConverter () =
     interface INumeralConverter with
         member this.ToNumeral number = Numeral.toFarsi number
         member this.TryParse (s, result) =
-            match s = null, lazy (Numeral.tryParseFarsi s) with
-            | true, _
-            | false, Lazy None -> false
-            | false, Lazy (Some i) -> result <- i; true
+            Helper.tryParse Numeral.tryParseFarsi (s, &result)
          
 type Numeral private () =
     static member val English = EnglishNumeralConverter () :> INumeralConverter
