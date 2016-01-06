df3098bf8c33404c58d47c62a3c9e3fe1d9a837c
Hadi Eskandari <h.eskandari@gmail.com>
2016-01-06 20:59:03 +1100

    Adding Farsi implementation

--- Numsense/Numeral.fs
+++ Numsense/Numeral.fs
@@ -3,8 +3,11 @@ module Ploeh.Numsense.Numeral
 
 open Ploeh.Numsense.InternalDsl
 
-let toDanish =        Danish.toDanishImp 1
-let tryParseDanish =  Danish.tryParseDanishImp
+let toDanish        = Danish.toDanishImp 1
+let tryParseDanish  =  Danish.tryParseDanishImp
 
-let toEnglish =       English.toEnglishImp
+let toEnglish       = English.toEnglishImp
 let tryParseEnglish = English.tryParseEnglishImp
+
+let toFarsi         = Farsi.toFarsiImp
+let tryParseFarsi   = Farsi.tryParseFarsiImp
