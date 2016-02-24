c7fe4f9515f102a906330003855ee55b6baaf6e6
Marcio Rinaldi <me@marciorinaldi.com>
2016-02-24 17:39:02 -0300

    Handled casing and whitespaces

--- Numsense/BrazilianPortuguese.fs
+++ Numsense/BrazilianPortuguese.fs
@@ -1,15 +1,15 @@
 ﻿module internal Ploeh.Numsense.BrazilianPortuguese
 
 let internal tryParseBrazilianImp (x : string) =
-    match x with
-    | "zero"   -> Some 0
-    | "um"     -> Some 1
-    | "dois"   -> Some 2
-    | "três"   -> Some 3
-    | "quatro" -> Some 4
-    | "cinco"  -> Some 5
-    | "seis"   -> Some 6
-    | "sete"   -> Some 7
-    | "oito"   -> Some 8
-    | "nove"   -> Some 9
+    match x.Trim().ToUpper() with
+    | "ZERO"   -> Some 0
+    | "UM"     -> Some 1
+    | "DOIS"   -> Some 2
+    | "TRÊS"   -> Some 3
+    | "QUATRO" -> Some 4
+    | "CINCO"  -> Some 5
+    | "SEIS"   -> Some 6
+    | "SETE"   -> Some 7
+    | "OITO"   -> Some 8
+    | "NOVE"   -> Some 9
     | _        -> None
