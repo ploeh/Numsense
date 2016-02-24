b34c9ed03e49b6d2f00f9019e34bab01419b74dc
Marcio Rinaldi <me@marciorinaldi.com>
2016-02-24 17:37:10 -0300

    Added one to nine

--- Numsense.UnitTests/BrazilianPortugueseExamples.fs
+++ Numsense.UnitTests/BrazilianPortugueseExamples.fs
@@ -4,7 +4,16 @@ open Xunit
 open Swensen.Unquote
 
 [<Theory>]
-[<InlineData("zero", 0)>]
+[<InlineData(  "zero", 0)>]
+[<InlineData(    "um", 1)>]
+[<InlineData(  "dois", 2)>]
+[<InlineData(  "três", 3)>]
+[<InlineData("quatro", 4)>]
+[<InlineData( "cinco", 5)>]
+[<InlineData(  "seis", 6)>]
+[<InlineData(  "sete", 7)>]
+[<InlineData(  "oito", 8)>]
+[<InlineData(  "nove", 9)>]
 let ``tryParseBrazilian returns correct result`` (portuguese, expected) =
     let actual = Numeral.tryParseBrazilian portuguese
     Some expected =! actual
--- Numsense/BrazilianPortuguese.fs
+++ Numsense/BrazilianPortuguese.fs
@@ -2,5 +2,14 @@
 
 let internal tryParseBrazilianImp (x : string) =
     match x with
-    | "zero" -> Some 0
-    | _ -> None
+    | "zero"   -> Some 0
+    | "um"     -> Some 1
+    | "dois"   -> Some 2
+    | "três"   -> Some 3
+    | "quatro" -> Some 4
+    | "cinco"  -> Some 5
+    | "seis"   -> Some 6
+    | "sete"   -> Some 7
+    | "oito"   -> Some 8
+    | "nove"   -> Some 9
+    | _        -> None
