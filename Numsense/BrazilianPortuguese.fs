a630e22d8e278daa1d1531fb0d460f03649da8a1
Marcio Rinaldi <me@marciorinaldi.com>
2016-02-24 21:53:12 -0300

    Added thirty

--- Numsense/BrazilianPortuguese.fs
+++ Numsense/BrazilianPortuguese.fs
@@ -5,31 +5,32 @@ open Ploeh.Numsense.InternalDsl
 let internal tryParseBrazilianImp (x : string) =
     let rec conv acc candidate =
         match candidate with
-        | ""                   -> Some acc
-        | StartsWith " "     t
-        | StartsWith "E"     t -> conv acc t
-        | "ZERO"               -> Some  (0  + acc)
-        | StartsWith "VINTE" t -> conv (20  + acc) t
-        | "DEZ"                -> Some (10  + acc)
-        | "ONZE"               -> Some (11  + acc)
-        | "DOZE"               -> Some (12  + acc)
-        | "TREZE"              -> Some (13  + acc)
-        | "CATORZE"            -> Some (14  + acc)
-        | "QUATORZE"           -> Some (14  + acc)
-        | "QUINZE"             -> Some (15  + acc)
-        | "DEZESSEIS"          -> Some (16  + acc)
-        | "DEZESSETE"          -> Some (17  + acc)
-        | "DEZOITO"            -> Some (18  + acc)
-        | "DEZENOVE"           -> Some (19  + acc)
-        | "UM"                 -> Some  (1  + acc)
-        | "DOIS"               -> Some  (2  + acc)
-        | "TRÊS"               -> Some  (3  + acc)
-        | "QUATRO"             -> Some  (4  + acc)
-        | "CINCO"              -> Some  (5  + acc)
-        | "SEIS"               -> Some  (6  + acc)
-        | "SETE"               -> Some  (7  + acc)
-        | "OITO"               -> Some  (8  + acc)
-        | "NOVE"               -> Some  (9  + acc)
+        | ""                    -> Some acc
+        | StartsWith " "      t
+        | StartsWith "E"      t -> conv acc t
+        | "ZERO"                -> Some  (0  + acc)
+        | StartsWith "VINTE"  t -> conv (20  + acc) t
+        | StartsWith "TRINTA" t -> conv (30  + acc) t
+        | "DEZ"                 -> Some (10  + acc)
+        | "ONZE"                -> Some (11  + acc)
+        | "DOZE"                -> Some (12  + acc)
+        | "TREZE"               -> Some (13  + acc)
+        | "CATORZE"             -> Some (14  + acc)
+        | "QUATORZE"            -> Some (14  + acc)
+        | "QUINZE"              -> Some (15  + acc)
+        | "DEZESSEIS"           -> Some (16  + acc)
+        | "DEZESSETE"           -> Some (17  + acc)
+        | "DEZOITO"             -> Some (18  + acc)
+        | "DEZENOVE"            -> Some (19  + acc)
+        | "UM"                  -> Some  (1  + acc)
+        | "DOIS"                -> Some  (2  + acc)
+        | "TRÊS"                -> Some  (3  + acc)
+        | "QUATRO"              -> Some  (4  + acc)
+        | "CINCO"               -> Some  (5  + acc)
+        | "SEIS"                -> Some  (6  + acc)
+        | "SETE"                -> Some  (7  + acc)
+        | "OITO"                -> Some  (8  + acc)
+        | "NOVE"                -> Some  (9  + acc)
         | _ -> None
 
     let canonicalized = x.Trim().ToUpper(System.Globalization.CultureInfo "pt-BR")
